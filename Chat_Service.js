0441fc2adf42a6ce32cd90f86e3251455963c48e
Tania16SW06 <44234142+Tania16SW06@users.noreply.github.com>
2018-12-01 01:02:13 +0500

    Add files via upload

---
+++ Chat_Service.js
@@ -0,0 +1,18 @@
+function ShowChat(){
+document.getElementById("Sidra").style.visibility="visible";
+}
+function ShowChat2(){
+
+	document.getElementById("Tania").style.visibility="visible";
+
+}
+function CloseChat(){
+
+	document.getElementById("Sidra").style.visibility="hidden";
+
+}
+function CloseChat2(){
+
+	document.getElementById("Tania").style.visibility="hidden";
+
+}
